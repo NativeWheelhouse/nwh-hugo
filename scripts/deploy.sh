d18df0ab27dd22ce757a5cb46ceaef3c547d5a81
Jake Adams <jsdev42@gmail.com>
2016-06-25 22:13:24 -0700

    regen deploy key the right way and update deployment script to properly decrypt key for deploying to github

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,15 +4,31 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Go To Public folder
 cd public
+
+# If there are no changes (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the static site on this push; exiting."
+    exit 0
+fi
+
+git config user.name "${COMMIT_AUTHOR}"
+git config user.email "${COMMIT_AUTHOR_EMAIL}"
+
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="deploying site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
+git commit -m "deploying site `date` ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
 
 # Push source and build repos.
 git push origin master
