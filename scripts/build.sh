2e18ebbaa98b02e923b087df7b4a2df8d06d6e2e
Jake Adams <jsdev42@gmail.com>
2016-06-26 13:53:42 -0700

    move commit to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,3 +4,23 @@ echo -e "Generating static site..."
 
 # Build the project.
 hugo
+
+SOURCE_BRANCH="master"
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    exit 0
+fi
+
+# Go To Public folder
+cd public
+
+SHA=`git rev-parse --verify HEAD`
+
+git config user.name "${COMMIT_AUTHOR}"
+git config user.email "${COMMIT_AUTHOR_EMAIL}"
+
+# Add changes to git and commit.
+git add .
+git commit -m "deploying site `date` ${SHA}"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,38 +11,28 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
-# Go To Public folder
-cd public
-git checkout $SOURCE_BRANCH
-
-#convert origin from https to ssh so the key will work
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
-git config user.name "${COMMIT_AUTHOR}"
-git config user.email "${COMMIT_AUTHOR_EMAIL}"
-
-# Add changes to git and commit.
-git add .
-git commit -m "deploying site `date` ${SHA}"
-git status
-
 # Get the deploy key by using Travis's stored variables to decrypt travisci.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../nwh_travisci.enc -out ../deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in nwh_travisci.enc -out deploy_key -d
 
-chmod 600 ../deploy_key
+chmod 600 deploy_key
 eval `ssh-agent -s`
-ssh-add ../deploy_key
+ssh-add deploy_key
 
 # Push source
+cd public
+
+#convert origin from https to ssh so the key will work
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 echo $SSH_REPO
 echo $SOURCE_BRANCH
+
+git config push.default simple
 git push $SSH_REPO $SOURCE_BRANCH
 
 # Come Back
