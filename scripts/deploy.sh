f754efa79ec6f0bc152c481473d0affb78c63a29
Jake Adams <jsdev42@gmail.com>
2016-06-25 23:31:20 -0700

    use a key without a passphrase

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
+set -e
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Go To Public folder
-cd public
+SOURCE_BRANCH="master"
 
-# If there are no changes (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the static site on this push; exiting."
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
     exit 0
 fi
 
-set -e # always exit with failure when something fails
+# Go To Public folder
+cd public
+git checkout $SOURCE_BRANCH
+
+#convert origin from https to ssh so the key will work
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
 git config user.name "${COMMIT_AUTHOR}"
 git config user.email "${COMMIT_AUTHOR_EMAIL}"
 
-# Add changes to git.
+# Add changes to git and commit.
 git add .
-
-# Commit changes.
 git commit -m "deploying site `date` ${SHA}"
+git status
 
 # Get the deploy key by using Travis's stored variables to decrypt travisci.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
@@ -27,14 +34,16 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../travisci.enc -out ../deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../nwh_travisci.enc -out ../deploy_key -d
 
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
 ssh-add ../deploy_key
 
-# Push source and build repos.
-git push origin master
+# Push source
+echo $SSH_REPO
+echo $SOURCE_BRANCH
+git push $SSH_REPO $SOURCE_BRANCH
 
 # Come Back
 cd ..
